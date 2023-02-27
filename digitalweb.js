68de67e19842a342829f189e61f44572a34bd405
StvyG <123375655+StvyG@users.noreply.github.com>
2023-02-27 11:46:07 +0000

    Add files via upload

---
+++ digitalweb.js
@@ -0,0 +1,29 @@
+console.log("Hello, world!");
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>About Digital Pictures</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <nav>
+      <a href="index.html">Home</a>
+      <a href="about.html">About</a>
+      <a href="services.html">Services</a>
+      <a href="contact.html">Contact Us</a>
+    </nav>
+    <h1>Our Services</h1>
+    <p>At Digital Pictures, we offer a variety of services to meet your digital needs. Here are some of the services we provide:</p>
+    <ul>
+      <li><strong>Illustration:</strong> Our team of talented illustrators can create stunning digital illustrations for your project, whether it's a book cover, advertising campaign, or anything else.</li>
+      <li><strong>Photo Editing:</strong> We can edit your digital photos to make them look their best, whether it's fixing color issues, removing blemishes, or adding special effects.</li>
+      <li><strong>Logo Design:</strong> If you're starting a new business or want to refresh your existing brand, we can design a logo that will make you stand out.</li>
+      <li><strong>Web Design:</strong> Our web designers can create a beautiful, functional website for your business or personal needs.</li>
+    </ul>
+    <h2>Get in touch</h2>
+    <p>If you're interested in any of our services or have questions, please don't hesitate to contact us.</p>
+    <script src="script.js"></script>
+  </body>
+</html>
+
+
